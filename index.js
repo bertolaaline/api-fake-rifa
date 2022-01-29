24ad46c2faa8ea20367fb27b7360ba29e190123b
Aline Cristina <bertola.aline@gmail.com>
2022-01-29 15:57:31 -0300

    Criando API

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express")
+
+const app = express()
+
+let quantidadeDeBilhetes = 1000
+
+const bilhetes = []
+
+const varianteDeStatus = [
+  "disponivel",
+  "reservado",
+  "vendido"
+]
+
+const sorteio = limite => Math.floor(Math.random() * limite)
+
+const geraBilhetes = () => {
+  for (let i = 0; i < quantidadeDeBilhetes; i++) {
+    const bilhete = {
+      numero: i + 1,
+      status: varianteDeStatus[sorteio(varianteDeStatus.length)]
+    }
+    bilhetes.push(bilhete)
+  }
+}
+
+geraBilhetes()
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    mensagem: "Hello World"
+  })
+})
+
+app.get("/bilhetes", (req,res) => {
+  res.status(200).json({
+    bilhetes: bilhetes
+  })
+})
+
+app.listen(3000)
